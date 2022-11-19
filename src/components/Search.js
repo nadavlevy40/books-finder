88e181668e1e7ba9e20a278a916bf0147975a489
unknown <nadavlevy40@gmail.com>
2022-11-19 15:21:40 +0200

    fixed

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,46 +2,30 @@ import {React,useState} from 'react';
 import axios from "axios";
 import Popup from "./Popup";
 
-
 function Search(){
-    const [searchInput, setSearchInput] = useState('');
+    const [searchInput, setSearchInput] = useState("");
     const [books, setBooks] = useState({ items: [] });
     
-
-    // const[book,setBook]=React.useState('');
-    // const[result,setResult]=React.useState([]);
-
-    function handleImgClicked(){
-
-    }
-
     function handleChange(event){
-            const book=event.target.value;
-            setSearchInput(book);
+            const input=event.target.value;
+    
+            setSearchInput(input);   
         }
-        
+
         let API_URL = `https://www.googleapis.com/books/v1/volumes`;
         const fetchBooks = async () => {
             const result = await axios.get(`${API_URL}?q=${searchInput}&maxResults=20`);
+            console.log(result);
             setBooks(result.data);
-            console.log(result.data);
-          };
 
-        
-           
-        
+          };
          function onSubmitHandler(event){
             event.preventDefault();
             fetchBooks();
+            
           };
-
-
-
-
-        
        const name=localStorage.getItem("username");
      
-    
         return (
         <section>
             <h1>welcome {name}</h1>
@@ -59,21 +43,21 @@ function Search(){
               <ul style={{ listStyle: 'none' }}>
                 {books.items.map((book, index) => {
                   return (
-                    <li key={index}>
+                    <li key={index}>    
+                      <div>
                       <div>
                         <img
                            alt="book"
-                           src={book.volumeInfo.imageLinks.thumbnail}
-                        //   //alt={`${book.volumeInfo.title} book`}
-                        //   //src={`http://books.google.com/books/content?id=${
-                        //     //book.id
-                        //   }&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+                            
+                           src={`http://books.google.com/books/content?id=${
+                    book.id
+                  }&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
                         />
                         <Popup authors={book.volumeInfo.authors}
                         categorys={book.volumeInfo.categories}
                         bookTitle={book.volumeInfo.title}
                         text="Hello there!" />
-                        
+                        </div>
                         <div>
                           <h3>{book.volumeInfo.title}</h3>
                           <p>{book.volumeInfo.publishedDate}</p>
@@ -87,17 +71,5 @@ function Search(){
             </section>
           );
         };
-
-//     <section>
-//     <h2>Book Search</h2>
-//     <input type="text" onChange={handleChange} value={searchInput}placeholder="Search for books"/>
-//     <button  onClick={handleClick}>Search</button>
-//    {books.map((book, i)=>{
-//               return <img key={i} src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
-             
-//    })}
-//     </section>
-//     );  
-// }
 
 export default Search;
